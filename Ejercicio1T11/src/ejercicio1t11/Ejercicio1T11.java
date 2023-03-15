fab8484b78589f11909bce174ba44d6652f73f67
vjp-hugoSC <hserranoc01@informatica.iesvalledeljerteplasencia.es>
2023-03-15 16:27:59 +0100

    ej 1

--- Ejercicio1T11/src/ejercicio1t11/Ejercicio1T11.java
+++ Ejercicio1T11/src/ejercicio1t11/Ejercicio1T11.java
@@ -33,71 +33,78 @@ public class Ejercicio1T11 {
         }
         return dia;
     }
-    public static void rellenarDia(Map<Integer, Dia> mapa){
-        for(int i=0;i<31;i++){
+
+    public static void rellenarDia(Map<Integer, Dia> mapa) {
+        for (int i = 0; i < 31; i++) {
             mapa.put(i, rellenarTemperaturas());
         }
     }
-    
-    public static void mostrarTemp(Map<Integer, Dia> mapa){
+
+    public static void mostrarTemp(Map<Integer, Dia> mapa) {
         for (Integer i : mapa.keySet()) {
-            System.out.println("Clave: "+i+ "Valor: "+mapa.get(i).toString());
+            System.out.println("Clave: " + i + " Valor: " + mapa.get(i).toString());
         }
     }
-    
-    public static void mostrarTempMedia(Map<Integer, Dia> mapa){
-        int suma=0;
+
+    public static void mostrarTempMedia(Map<Integer, Dia> mapa) {
+        int suma = 0;
         int media;
         for (int i = 0; i < mapa.size(); i++) {
-            suma+=mapa.get(i).getTemperatura();
+            suma =suma+mapa.get(i).getTemperatura();
         }
-        media=suma/mapa.size();
+        media = suma / mapa.size();
     }
-    
-    public static void diaCaluroso(Map<Integer, Dia> mapa){
-        int tempMax=mapa.get(0).getTemperatura();
+
+    public static void diaCaluroso(Map<Integer, Dia> mapa) {
+        int tempMax = mapa.get(0).getTemperatura();
         for (Integer m : mapa.keySet()) {
-            if (mapa.get(m).getTemperatura()>tempMax) {
-                tempMax=mapa.get(m).getTemperatura();
+            if (mapa.get(m).getTemperatura() > tempMax) {
+                tempMax = mapa.get(m).getTemperatura();
             }
         }
-        
+
         for (Integer i : mapa.keySet()) {
-            if (mapa.get(i).getTemperatura()==tempMax) {
-                System.out.println("El dia mas caluroso feu "+mapa.get(i).getNombredia()+" dia "+i+"con"+tempMax+"grados");
+            if (mapa.get(i).getTemperatura() == tempMax) {
+                System.out.println("El dia mas caluroso feu " + mapa.get(i).getNombredia() + " dia " + i + "con" + tempMax + "grados");
             }
         }
     }
-     /**
+
+    /**
      * @param args the command line arguments
      */
 
     public static void main(String[] args) {
-        Scanner entrada=new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         Map<Integer, Dia> mapa = new HashMap<>(31);
-       
+
         int opc;
-       
-        do{
+
+        do {
             System.out.println("1.Rellenar las temperaturas:");
             System.out.println("2.Mostrar las temperaturas:");
             System.out.println("3.Visualizar la temperatura media:");
             System.out.println("4.Dia o días más calurosos del mes");
             System.out.println("5.Salir del programa");
-            System.out.println("===OPCION===");
-            opc=entrada.nextInt();
-            switch(opc){
+            System.out.println("-----Seleciona la opcion que quieras-----");
+            System.out.println("-----------------------------------------");
+            opc = entrada.nextInt();
+            switch (opc) {
                 case 1:
                     rellenarDia(mapa);
+                    System.out.println("\n");
                     break;
                 case 2:
                     mostrarTemp(mapa);
+                    System.out.println("\n");
                     break;
                 case 3:
                     mostrarTempMedia(mapa);
+                    System.out.println("\n");
                     break;
                 case 4:
                     diaCaluroso(mapa);
+                    System.out.println("\n");
                     break;
                 case 5:
                     System.out.println("Has salido del programa.");
@@ -105,9 +112,7 @@ public class Ejercicio1T11 {
                 default:
                     System.out.println("Por favor, pon un numero entre el 1 y 5");
             }
-        }while(opc != 5);
-        
+        } while (opc != 5);
+
     }
 }
-    
-
